c9f17c7ae76c8a662b27f652a2cacdbbfa0e50cd
Dejan Krajisnik <krajisnikdejan@gmail.com>
2024-10-25 12:28:59 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = axios("http://localhost:3005/");
+
+      const data = await response;
+
+      setCount(data.data.length);
+      console.log(data);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -18,8 +32,13 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button
+          onClick={() => {
+            console.log(count, "aaaaa");
+            setCount((count) => count + 1);
+          }}
+        >
+          count is <code>{count}</code>
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
@@ -29,7 +48,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
